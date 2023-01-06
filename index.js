9ce79fb73dee7597a7364411a8b39b0ac6665ca5
RegisLewis <regisaslewis@gmail.com>
2023-01-05 22:20:25 -0800

    commit tree dad82341128e94c28189cf377d2bf411c8b4ac58

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = [
+    "Milo", "Otis", "Garfield"
+];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+
+};
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+};
+
+function appendCat(name) {
+    const newCatsArray = [...cats, name];
+    return newCatsArray;
+};
+
+function prependCat(name) {
+    const secondNewCatsArray = [name, ...cats];
+    return secondNewCatsArray;
+};
+
+function removeLastCat() {
+    const lastCat = cats.slice(0, -1);
+    return lastCat;
+};
+
+function removeFirstCat() {
+    const firstCat = cats.slice(1);
+    return firstCat;
+};
